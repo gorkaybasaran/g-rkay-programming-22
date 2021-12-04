40258e8e72e2f52eefcffa4b63d594db657d9a84
gorka <gorka@DESKTOP-902BBMO>
2021-12-04 20:57:34 +0100

    Proje dosyası ekle.

---
+++ görkay programming 22/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("hello stranger,let me know you :) ");
+
+
+Console.WriteLine("what is your name");
+string name = Console.ReadLine();
+
+Console.WriteLine("what is your lastname");
+string lastname = Console.ReadLine();
+
+Console.WriteLine("what is your street");
+string street = Console.ReadLine();
+
+Console.WriteLine("what is your house number");
+string number = Console.ReadLine();
+
+Console.WriteLine("what is your zip code");
+string zip = Console.ReadLine();
+
+Console.WriteLine("what is your city");
+string city = Console.ReadLine();
+
+Console.WriteLine("your name" + name);
+Console.WriteLine("your last name " + lastname);
+Console.WriteLine("your street " + street);
+Console.WriteLine("your house number " + number);
+Console.WriteLine("your zip code " + zip);
+Console.WriteLine("your city " + city);
